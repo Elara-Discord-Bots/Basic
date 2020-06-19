afb902cd2b40cb115beffa3f2ef249d106ef9ee5
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2020-06-18 21:29:44 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,27 @@
 const {CommandoClient} = require('discord.js-commando'),
       client = new CommandoClient({
-        owner: ["Bot Dev id"],
+        owner: ["The bot owner's user ID"],
         commandPrefix: "!",
         fetchAllMembers: true,
         commandEditableDuration: 10000,
         unknownCommandResponse: false,
         invite: "https://discord.gg/InviteHere",
       });
-client.registry.registerDefaultGroups().registerGroups([["basic", "Basic Commands"]]).registerDefaultCommands({ping: true, prefix: true, help: true, commandState: true, eval_: true}).registerDefaultTypes().registerCommandsIn(require('path').join(__dirname, 'commands'))
+
+
+client.registry
+      .registerDefaultGroups()
+      .registerGroups([
+      ["basic", "Basic Commands"]
+      ])
+      .registerDefaultCommands({
+            ping: true, 
+            prefix: true, 
+            help: true, 
+            commandState: true, 
+            eval_: true
+      })
+      .registerDefaultTypes()
+      .registerCommandsIn(require('path').join(__dirname, 'commands'))
+
 client.login("--TOKEN--")
